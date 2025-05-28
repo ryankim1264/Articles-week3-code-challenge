682a56f47700e0e89a80b986490e42138a012459
Ryan mugei <Lquid1264@gmail.com>
2025-05-28 16:08:29 +0300

    aricles

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -3,5 +3,5 @@ import sqlite3
 
 def get_connection():
     conn = sqlite3.connect('articles.db')
-    conn.row_factory = sqlite3.Row # This enables column access by name
+    conn.row_factory = sqlite3.Row 
     return conn
